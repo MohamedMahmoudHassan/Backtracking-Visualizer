42a8201af6cf8b544e95fab3888a9a2ba2640af0
Mohamed Mahmoud <mohamedmahmoudalihassan@gmail.com>
2021-12-12 21:48:35 +0200

    serve-static was misspelled

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const serverStatic = require("server-static");
+const serveStatic = require("serve-static");
 const path = require("path");
 
 const app = express();
 
-app.use("/", serverStatic(path.join(__dirname, "/dist")));
+app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
 const port = process.env.PORT || 8080;
 app.listen(port);
-console.log("Listening to port" + port + "...");
+console.log("Listening to port " + port + "...");
